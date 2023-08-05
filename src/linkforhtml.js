7fe90ccb0d4d7a11fd455f5fcf32973074135c32
react <anandhan04040@gmail.com>
2023-08-05 11:34:48 +0530

    third

--- src/linkforhtml.js
+++
@@ -1,6 +0,0 @@
-function me(){
-    document.getElementById("first").innerHTML="naan than"
-    document.getElementById("second").innerHTML="unna than"
-    // document.write("web page kanama pochuuu..")
-}
-
